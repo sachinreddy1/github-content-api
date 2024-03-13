ba9373f847175b53135b2476dce3a4ddbe73ca3c
Sachin Reddy <sachinreddy1009@gmail.com>
2024-03-13 14:34:30 -0400

    Fixed output document formatting

--- github-content-api/src/main/kotlin/com/sachinreddy/githubcontentapi/service/GitHubContentService.kt
+++ github-content-api/src/main/kotlin/com/sachinreddy/githubcontentapi/service/GitHubContentService.kt
@@ -43,21 +43,31 @@ class GitHubContentService @Autowired constructor(
     }
 
     private fun getContentString(gitHubTreeResponse: GitHubTreeResponse): String {
-        var ret = ""
+        var ret = "<documents>\n"
+        var index = 1
+
         gitHubTreeResponse.tree.forEach {
             val blob = getGitHubBlob(it.url)
 
             try {
                 blob.content?.let { blobContent ->
+                    ret += "<document index=\"${index}\">\n"
+                    ret += "<source>\n"
                     ret += "${it.path}\n"
-                    ret += "\n"
+                    ret += "</source>\n"
+                    ret += "<document_content>\n"
                     ret += "${decodeBase64(blobContent.replace("\n", ""))}\n"
-                    ret += "---------------\n"
+                    ret += "</document_content>\n"
+                    ret += "</document>\n"
+
+                    index += 1
                 }
             } catch (e: Exception) {
 
             }
         }
+
+        ret += "</documents>\n"
 
         return ret
     }
